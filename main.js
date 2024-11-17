168b70068fe61d581d220cea471aec05505f2a19
Orashi6 <bekb0323@gmail.com>
2024-11-17 10:39:03 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+let savol1 = prompt('string typedagi malumotni kiriting')
+let savol2 = prompt('number typedagi malumot kiriting')
+let savol3 = prompt('boolean typedagi malumot kiriting')
+
+
+
+let negaIshlamapti = alert('siz kiritgan malumot  '  +  savol1 +  typeof (savol1) + `tipiga tegishli  ` +
+    
+    `siz kiritgan malumot    ${savol2}  ${typeof(Number(savol2))}   tipiga tegishli  `
+   
+    +
+   
+   ` siz kiritgan malumot    ${savol3}  ${typeof(Boolean(savol3))}  tipiga tegishli ` 
+  
+
+);
+
+
+console.log(negaIshlamapti);
